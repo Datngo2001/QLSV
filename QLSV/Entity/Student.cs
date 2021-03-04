26f1286d0e45d6ad817b28ad246369ec5ceff2b8
Ngô Đạt <supodatngo123@gmail.com>
2021-03-04 15:10:00 +0700

    create student class

---
+++ QLSV/Entity/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using QLSV.Data;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace QLSV.Entity
+{
+    class Student
+    {
+        DataBase mydb = new DataBase();
+        public bool insertStudent(int Id, string fname, string lname, DateTime date, string gender, string phone, string address, MemoryStream picture)
+        {
+            SqlCommand command = new SqlCommand("INSERT INTO std (id, fname, Iname, bdate, gender, phone, " +
+                "VALUES (@id, @fn, @ln, @bdt, @gd, @phn, @adrs, @pic)", mydb.Connection);
+
+            command.Parameters.Add("@id", SqlDbType.Int).Value = Id;
+            command.Parameters.Add("@fn", SqlDbType.VarChar).Value = fname;
+            command.Parameters.Add("@ln", SqlDbType.VarChar).Value = lname;
+            command.Parameters.Add("@bdt", SqlDbType.DateTime).Value = date;
+            command.Parameters.Add("@gd", SqlDbType.VarChar).Value = gender;
+            command.Parameters.Add("@ohn", SqlDbType.VarChar).Value = phone;
+            command.Parameters.Add("@adrs", SqlDbType.VarChar).Value = address;
+            command.Parameters.Add("@pic", SqlDbType.Image).Value = picture.ToArray();
+
+            mydb.openConnection();
+
+            if ((command.ExecuteNonQuery() == 1))
+            {
+                mydb.closeConnection();
+                return true;
+            }
+            else
+            {
+                mydb.closeConnection();
+                return false;
+            }
+        }
+    }
+}
--- QLSV/MainForm.Designer.cs
+++ QLSV/MainForm.Designer.cs
@@ -32,44 +32,112 @@ namespace QLSV
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.Dropdown_Student = new System.Windows.Forms.ToolStripMenuItem();
             this.item_NewStudent = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOURSEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sCOREToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.staticsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editRemoveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageStudentFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.Dropdown_Student});
+            this.Dropdown_Student,
+            this.cOURSEToolStripMenuItem,
+            this.sCOREToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1077, 46);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(969, 46);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "STUDENT";
             // 
             // Dropdown_Student
             // 
             this.Dropdown_Student.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.item_NewStudent});
-            this.Dropdown_Student.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.item_NewStudent,
+            this.studentListToolStripMenuItem,
+            this.staticsToolStripMenuItem,
+            this.editRemoveToolStripMenuItem,
+            this.manageStudentFormToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.Dropdown_Student.Font = new System.Drawing.Font("Segoe UI", 14F);
             this.Dropdown_Student.Name = "Dropdown_Student";
             this.Dropdown_Student.Size = new System.Drawing.Size(152, 42);
             this.Dropdown_Student.Text = "STUDENT";
             // 
             // item_NewStudent
             // 
+            this.item_NewStudent.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.item_NewStudent.Name = "item_NewStudent";
-            this.item_NewStudent.Size = new System.Drawing.Size(341, 46);
+            this.item_NewStudent.Size = new System.Drawing.Size(399, 46);
             this.item_NewStudent.Text = "Add New Student";
             this.item_NewStudent.Click += new System.EventHandler(this.addStudent_Clicked);
             // 
+            // cOURSEToolStripMenuItem
+            // 
+            this.cOURSEToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cOURSEToolStripMenuItem.Name = "cOURSEToolStripMenuItem";
+            this.cOURSEToolStripMenuItem.Size = new System.Drawing.Size(135, 42);
+            this.cOURSEToolStripMenuItem.Text = "COURSE";
+            // 
+            // sCOREToolStripMenuItem
+            // 
+            this.sCOREToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sCOREToolStripMenuItem.Name = "sCOREToolStripMenuItem";
+            this.sCOREToolStripMenuItem.Size = new System.Drawing.Size(116, 42);
+            this.sCOREToolStripMenuItem.Text = "SCORE";
+            // 
+            // studentListToolStripMenuItem
+            // 
+            this.studentListToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.studentListToolStripMenuItem.Name = "studentListToolStripMenuItem";
+            this.studentListToolStripMenuItem.Size = new System.Drawing.Size(399, 46);
+            this.studentListToolStripMenuItem.Text = "Student List ";
+            // 
+            // staticsToolStripMenuItem
+            // 
+            this.staticsToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.staticsToolStripMenuItem.Name = "staticsToolStripMenuItem";
+            this.staticsToolStripMenuItem.Size = new System.Drawing.Size(399, 46);
+            this.staticsToolStripMenuItem.Text = "Statics";
+            // 
+            // editRemoveToolStripMenuItem
+            // 
+            this.editRemoveToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.editRemoveToolStripMenuItem.Name = "editRemoveToolStripMenuItem";
+            this.editRemoveToolStripMenuItem.Size = new System.Drawing.Size(399, 46);
+            this.editRemoveToolStripMenuItem.Text = "Edit / Remove";
+            // 
+            // manageStudentFormToolStripMenuItem
+            // 
+            this.manageStudentFormToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.manageStudentFormToolStripMenuItem.Name = "manageStudentFormToolStripMenuItem";
+            this.manageStudentFormToolStripMenuItem.Size = new System.Drawing.Size(399, 46);
+            this.manageStudentFormToolStripMenuItem.Text = "Manage Student Form";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(399, 46);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1077, 633);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(969, 506);
             this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "19110115 Ngô Minh Đạt 19110101 Nguyễn Thành Phát";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -81,5 +149,12 @@ namespace QLSV
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem Dropdown_Student;
         private System.Windows.Forms.ToolStripMenuItem item_NewStudent;
+        private System.Windows.Forms.ToolStripMenuItem studentListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem staticsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editRemoveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageStudentFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOURSEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sCOREToolStripMenuItem;
     }
 }
